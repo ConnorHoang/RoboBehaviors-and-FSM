3d065722a2cb4cbe196b4e9f62ac7829d36231c6
Connor Hoang <connorhoang3@gmail.com>
2025-09-19 14:59:27 -0400

    merging e_stop

--- ros_behaviors_fsm/ros_behaviors_fsm/wall_follow.py
+++ ros_behaviors_fsm/ros_behaviors_fsm/wall_follow.py
@@ -11,13 +11,15 @@ from rclpy.parameter import Parameter
 from rcl_interfaces.msg import SetParametersResult
 from rclpy.qos import qos_profile_sensor_data
 
-class WallApproachNode(Node):
+class NeatoFsm(Node):
     """ This class wraps the basic functionality of the node """
     def __init__(self):
-        super().__init__('wall_approach')
+        super().__init__('neato_fsm')
         # the run_loop adjusts the robot's velocity based on latest laser data
         self.create_timer(0.1, self.run_loop)
+        # Subscriber to intake laser sensor data
         self.create_subscription(LaserScan, 'scan', self.process_scan, qos_profile=qos_profile_sensor_data)
+        # publisher to send Neato velocity to ROS space
         self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
         # distance_to_obstacle is used to communciate laser data to run_loop
         self.distance_to_obstacle = None
@@ -27,15 +29,19 @@ class WallApproachNode(Node):
         self.target_distance = 1.2
         # value to trigger stop in driving
         self.stop = False
-        # forward velocity of neato
-        self.vel = 0.5
+        # value for forward velocity of neato
+        self.vel = 0.3
+        # velocity (speed and angle) of Neato
+        self.velocity = Twist()
+        # bump boolean for physical sensor where True is bumped
+        self.bumped = False
 
     def run_loop(self):
-        """Reset drive and then drive unless told to stop"""
+        """Primary loop"""
         self.drive(linear=0.0, angular=0.0)
 
         if not self.stop:
-            self.drive(linear=0.5, angular=0.0)
+            self.drive(self.velocity, linear=0.5, angular=0.0)
             sleep(0.1)
 
     def process_scan(self, msg):
@@ -47,23 +53,58 @@ class WallApproachNode(Node):
 
             print('scan received', msg.ranges[0])
 
-    def drive(self, linear, angular):
+    def drive(self, msgArg, linear, angular):
         """Drive with the specified linear and angular velocity.
 
         Args:
             linear (_type_): the linear velocity in m/s
             angular (_type_): the angular velocity in radians/s
         """        
-        msg = Twist()
+        msg = msgArg
         msg.linear.x = linear
         msg.angular.z = angular
         self.vel_pub.publish(msg)
 
+    def process_bump(self, msg):
+        """Callback for handling a bump sensor input."
+        Input: 
+            msg (Bump): a Bump type message from the subscriber.
+        """
+        # Set the bump state to True if any part of the sensor is pressed
+        self.bumped = (msg.left_front == 1 or \
+                           msg.right_front == 1 or \
+                           msg.left_side == 1 or \
+                           msg.right_side == 1)
+
 def main(args=None):
     rclpy.init(args=args)
-    node = WallApproachNode()
+    node = NeatoFsm()
     rclpy.spin(node)
     rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+# retain info when transitioning between states to modify state behavior?
+
+
+""" 
+
+state: wall_approach
+- drive fowrward
+t: at wall -> move to turn
+
+state: wall_follow
+- drive forward
+- if decreasing in distance to wall, angle out of wall
+- if increasing in distance to wall, andle into wall
+
+state: turn
+- drive forward
+- turn away from wall
+
+"""
